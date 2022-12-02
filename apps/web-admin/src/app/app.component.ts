3582d05fe09bbc63304a358d70cd6d313455f5d3
Tony Tran <tonytran20081997@gmail.com>
2022-12-03 00:41:21 +0700

    - new source

---
+++ apps/web-admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eng-cards-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web-admin';
+}
